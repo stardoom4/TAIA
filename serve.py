0f0c55f1b72efb59ebcfd9179f8cc7847dec75e7
Celestial-Entity <139852159+stardoom4@users.noreply.github.com>
2024-10-26 00:48:08 +0530

    Update serve.py

--- serve.py
+++ serve.py
@@ -132,7 +132,7 @@ def generate_microblog_page(microblog_entries, entries, output_dir):
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Microblog - Page {page_num}</title>
+<title>Wunder</title>
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
